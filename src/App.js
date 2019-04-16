37d96cbe8639bd8c7dd8466721dc46352ad09ed0
Thomas Kwak <teekwak@gmail.com>
2019-04-16 16:56:19 -0700

    Print names of all tabs to console

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from 'react';
+/* global chrome */
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 // cool idea, badge could be number of total tabs open!
 export default function App() {
+  useEffect(() => {
+    chrome.tabs.query({}, tabs => {
+      tabs.forEach(tab => {
+        console.log(tab.title);
+      })
+    })
+  });
+
   return (
     <div className="App">
       <header className="App-header">
