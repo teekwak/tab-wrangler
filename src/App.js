6a2cef99add1bd107e5d73a97b496779b25d762f
Thomas Kwak <teekwak@gmail.com>
2019-04-17 10:01:32 -0700

    Move sorting to separate function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,46 @@
 /* global chrome */
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 // cool idea, badge could be number of total tabs open!
+// filtering function (fuzzy search...)
+
+function sortByTitleAndURL(a, b) {
+  const aTitle = a.title.toLowerCase();
+  const bTitle = b.title.toLowerCase();
+
+  if (aTitle < bTitle) {
+    return -1;
+  } else if (aTitle > bTitle) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+function filterByText(tab, text) {
+  return tab.title.toLowerCase().startsWith(text);
+}
+
 export default function App() {
   const [text, setText] = useState('');
   const [tabs, setTabs] = useState([]);
 
   useEffect(() => {
     chrome.tabs.query({}, result => {
-      result.sort((a, b) => {
-        if(a.title < b.title) {
-          return -1
-        } else if (a.title > b.title) {
-          return 1
-        } else {
-          return 0
-        }
-      });
-
+      result.sort(sortByTitleAndURL);
       setTabs(result);
     })
   }, []);
 
   function handleTextChange(event) {
-    setText(event.target.value);
+    setText(event.target.value.toLowerCase());
   }
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -43,12 +53,13 @@ export default function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
       <input
+        autoFocus
         type="text"
         onChange={handleTextChange}
       />
-      {tabs.filter(tab => tab.title.startsWith(text)).map(tab => <div>{tab.title}</div>)}
+      {tabs.filter(tab => filterByText(tab, text)).map(tab => <div>{tab.title}</div>)}
     </div>
   );
 }
